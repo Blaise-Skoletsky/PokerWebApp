d99051f2d67d906a6bed0fef9cbe480ccf58f863
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2023-11-16 22:23:10 -0800

    Update client.js

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ const socket = io('http://localhost:3000');
 var input = document.getElementById('textinput');
 
 
-
+//When the client recives the wasclicked message, data sent is stored in arg. 
 socket.on('wasclicked', function(arg){
     console.log(arg)
 
